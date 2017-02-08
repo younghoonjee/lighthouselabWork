07103e6328522453abe50027503fe86a0d217789
YounghoonJee <yjee@sfu.ca>
2017-02-08 11:53:12 -0800

    first commit

---
+++ Documents/LighthouseLab work /Contact List/Contact List/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Contact List
+//
+//  Created by Younghoon jee on 2017. 2. 8..
+//  Copyright © 2017년 Younghoon jee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
