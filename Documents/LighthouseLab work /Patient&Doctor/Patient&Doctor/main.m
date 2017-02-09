6c61b651076a61724943e1f6e85aee450dcc3771
YounghoonJee <yjee@sfu.ca>
2017-02-09 13:24:36 -0800

    first commit

---
+++ Documents/LighthouseLab work /Patient&Doctor/Patient&Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient&Doctor
+//
+//  Created by Younghoon jee on 2017. 2. 9..
+//  Copyright © 2017년 Younghoon jee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
